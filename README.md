57038b98c4b94cd8e34e6bf6e267e3c638164989
agourk <vascoagourk@gmail.com>
2022-03-10 22:48:16 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Crabada click bot
+
+## What?
+This is a Chrome extension that clicks through the Crabada idle game to find a mine to loot. Then the user has to complete a Captcha and send the loot transaction.
+
+## How?
+### Requirements:
+* Clone this repository somewhere
+* Activate "Developer mode" here: `chrome://extensions/`
+* That reveals the "Load unpacked" button, click it and indicate the `crabadaClickBot` you cloned in
+
+You should now have the extension available on your browser. To use it, go to https://play.crabada.com/mine/start-looting and click the extension.
+Choose the mines you wish to loot and click `Start`.
---
+++ content.js
@@ -0,0 +1,129 @@
+const LOOT_MINES = []
+const FIRST_MINES = []
+
+//Observes loading of main page
+const page_observer = new MutationObserver(mutations => {
+  mutations.forEach(mutation => {
+    if (mutation.addedNodes.length) {
+      if (document.evaluate("//div[text()='Start Looting']", document.body, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue) {
+        page_observer.disconnect()
+        refresh_first_mines()
+      }
+    }
+  })
+})
+
+//Observes loading of mines
+const first_mines_observer = new MutationObserver(mutations => {
+  mutations.forEach(mutation => {
+    for (let i = 0; i < mutation.addedNodes.length; i++) {
+      if (mutation.addedNodes[i].className && 'row no-result'.includes(mutation.addedNodes[i].className)) {
+        first_mines_observer.disconnect()
+        set_first_mines()
+      }
+    }
+  })
+})
+
+//Observes loading of mines
+const mines_observer = new MutationObserver(async mutations => {
+  mutations.forEach(async mutation => {
+    for (let i = 0; i < mutation.addedNodes.length; i++) {
+      if (mutation.addedNodes[i].className && 'row no-result'.includes(mutation.addedNodes[i].className)) {
+        await check_mines()
+      }
+    }
+  })
+})
+
+//Observes loading of "attack" button
+const attack_observer = new MutationObserver(mutations => {
+  mutations.forEach(mutation => {
+    for (let i = 0; i < mutation.addedNodes.length; i++) {
+      if (mutation.addedNodes[i].className == 'mine-detail') {
+        attack_observer.disconnect()
+        select_observer.observe(document.body, {childList: true, subtree: true})
+        document.getElementById('attackBtn').click()
+      }
+    }
+  })
+})
+
+//Observes loading of "select" button
+const select_observer = new MutationObserver(mutations => {
+  mutations.forEach(async mutation => {
+    for (let i = 0; i < mutation.addedNodes.length; i++) {
+      if (mutation.addedNodes[i].className == 'ant-modal-root') {
+        select_observer.disconnect()
+        await new Promise(r => setTimeout(r, 500))
+        try {
+          document.evaluate("//span[text()='Select']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.parentElement.click()
+        } catch {}
+      }
+    }
+  })
+})
+
+//Get first mines to not loot them
+async function refresh_first_mines() {
+  first_mines_observer.observe(document.getElementsByClassName('mine')[0], {childList: true, subtree: true})
+  document.evaluate("//div[text()='Start Looting']", document.body, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click()
+}
+
+//Set `first_mines` to not loot them
+async function set_first_mines() {
+  const mines = document.querySelectorAll('div.items-icon')
+  mines.forEach(mine => {
+    FIRST_MINES.push(mine.parentElement.textContent.split('\u00A0')[1].split(' ')[0])
+  })
+  mines_observer.observe(document.getElementsByClassName('mine')[0], {childList: true, subtree: true})
+  document.evaluate("//div[text()='Start Looting']", document.body, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click()
+}
+
+//Infinite loop that refreshes available mines and clicks the correct ones
+async function check_mines() {
+  const mines = document.querySelectorAll('div.items-icon')
+  await new Promise(r => setTimeout(r, 100))
+  for (let i = 0; i < 7 && i < mines.length; ++i) {
+    const mine_id = mines[i].parentElement.textContent.split('\u00A0')[1].split(' ')[0]
+    const mine_type = mines[i].firstChild.getAttribute('data-tooltip')
+    if (!FIRST_MINES.includes(mine_id) && LOOT_MINES.includes(mine_type)) {
+      mines_observer.disconnect()
+      attack_observer.observe(document.getElementsByClassName('mine')[0], {childList: true, subtree: true})
+      mines[i].click()
+      console.log('Crabada clicker bot ended')
+      return
+    }
+  }
+  document.evaluate("//div[text()='Start Looting']", document.body, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click()
+}
+
+//Waits for extension to be clicked
+chrome.runtime.onMessage.addListener(async function(msg, sender, sendResponse) {
+  if (msg.action === 'startBot') {
+    if (window.location.href != 'https://play.crabada.com/mine/start-looting') {
+      sendResponse('KO')
+      return
+    }
+    Object.keys(msg.types).forEach(type => {
+      if (msg.types[type].selected == true)
+        LOOT_MINES.push(type)
+      else {
+        const index = LOOT_MINES.indexOf(type)
+        if (index >= 0)
+          LOOT_MINES.splice(index, 1)
+      }
+    })
+    if (LOOT_MINES.length == 0) {
+      sendResponse('NO_TYPES')
+      return
+    }
+    console.log('Crabada clicker bot started')
+    if (document.evaluate("//div[text()='Start Looting']", document.body, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue) {
+      refresh_first_mines()
+    } else {
+      page_observer.observe(document, {childList: true, subtree: true})
+    }
+    sendResponse('OK')
+  }
+})
